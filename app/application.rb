5a62021b6ff743e01b0e2822900200c415f3c5f1
James Pier <jpier2012@gmail.com>
2019-05-25 00:15:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-       if @@items
+       if @@cart
          resp.write("#{@@items}")
        elsif @@items.empty?
           resp.write("Your cart is empty")
